f24261d26271cc64893fba24310c33c0e53a6e6d
kibria7533 <tenminuteversity@gmail.com>
2022-02-07 21:14:14 +0600

    edit compleat

--- src/App.js
+++ src/App.js
@@ -5,27 +5,41 @@ import Navber from "./Component/Navber";
 import { BrowserRouter , Routes, Route, Link } from "react-router-dom";
 import Form from "./Component/Form";
 import { useState } from "react";
+import EditForm from "./Component/EditForm";
 function App() {
-const [data,setData]=useState([]);
-
-const FetchData=(values)=>{
-setData([...data,values]);
+const [tt,setTT]=useState([])
+const getData=(data)=>{
+setTT([...tt,data])
 }
 
-const Delete=(id)=>{
-  //[{23},{45},{65}]-->2
-  let deletedData=data.filter((eld,indd)=> indd!=id);
-  setData(deletedData);
-  
-
+const deleteData=(id)=>{
+console.log(id);
+const filterd=tt.filter((a,b)=>b!=id);
+setTT(filterd)
 }
+
+const UpdateData=(up,id)=>{
+// console.log(up,'lll')
+let ans=tt.map((el,ind)=>{
+  if(ind==id){
+return up;
+  }
+  else{
+return el;
+  }
+})
+
+setTT(ans)
+}
+
 
   return (
     <BrowserRouter>
     <Navber/>
       <Routes>
-        <Route  path="/" element={<Home datas={data} DeleteFn={Delete} />} />
-        <Route   path="/add" element={<Form fetch={FetchData} />} />
+        <Route  path="/" element={<Home data={tt} deleteFn={deleteData}/>} />
+        <Route   path="/add" element={<Form fn={getData} />} />
+        <Route   path="/edit/:id" element={<EditForm up={UpdateData} data={tt}/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/Component/EditForm.js
+++ src/Component/EditForm.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState ,useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
-
-const Form = (props) => {
+const EditForm = ({data,up}) => {
 const [text,setText]=useState('');
+const {id}=useParams();
 const navigate=useNavigate();
+useEffect(()=>{
+let myData=data[id];
+setText(myData)
 
-const sendDatatoApp=()=>{
-    props.fn(text)
+},[])
+
+
+const upDate=()=>{
+    up(text,id);
     navigate('/')
 }
 
@@ -16,10 +22,10 @@ const sendDatatoApp=()=>{
       <div className="row ">
         <div className="col-md-12 justify-content-center text-center"></div>
         <input value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text"/>
-        <button type="button" onClick={sendDatatoApp} style={{width: '50px'}}>Save</button>
+        <button type="button" onClick={upDate} style={{width: '50px'}}>update</button>
       </div>
     </div>
   );
 };
 
-export default Form;
+export default EditForm;
